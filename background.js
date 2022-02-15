500d01f1c4ef7c7ca8be313a6b1b810355b733b4
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-02-15 12:55:44 +0100

    feat: add optional notification on first vist fix: increase canvas text size for icon

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 
+//const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+
 function shortTextForNumber (number) {
 	if (number < 1000) {
 		return number.toString()
@@ -14,13 +18,11 @@ function shortTextForNumber (number) {
 	}
 }
 
-
-
 function getIconImageData(rank) {
     const imageWidth = 42;
     const imageHeight = 42;
     const markerSize = 8;
-    const font = "bold 18pt 'Arial'";
+    const font = "bold 24pt 'Arial'";
     const color = "#000000";
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
@@ -45,13 +47,35 @@ function getIconImageData(rank) {
 
 async function handleUpdated(tabId, changeInfo, tabInfo) {
     if (changeInfo.url) {
-        //console.log("Tab: " + tabId + " URL changed to " + changeInfo.url);
-
         const visits = await browser.history.getVisits({ url:  changeInfo.url  });
+        const visits_count = visits.length < 1? 1: visits.length;
         browser.browserAction.setIcon({
-            'imageData': getIconImageData(visits.length)
+            'imageData': getIconImageData(visits_count)
             ,'tabId': tabId
         });
+        browser.browserAction.setTitle({
+            'title': 'Site Visits: ' + visits_count
+            ,'tabId': tabId
+        });
+        if(visits_count === 1){
+            // show a notification
+            //
+            const notify_title = 'First Visit';
+            const notify_message = 'FYI: This is the first time you visited this website';
+            var notify_permissions = {
+                permissions: ["notifications" ]
+            };
+            if (await browser.permissions.contains(notify_permissions)) {
+                browser.notifications.create(extname, {
+                    "type": "basic",
+                    "iconUrl": browser.runtime.getURL("icon.png"),
+                    "title": notify_title,
+                    "message":  notify_message
+                });
+            }
+
+
+        }
     }
 }
 
